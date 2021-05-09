56536f1cbf66b39bae576f6548e145758e451b30
VANDY <vandy.fathi@gmail.com>
2021-05-09 18:30:46 -0400

    réglage problèmes

--- WebApplication1/Controllers/AdminController.cs
+++ WebApplication1/Controllers/AdminController.cs
@@ -56,45 +56,6 @@ namespace WebApplication1.Controllers
             return View(db.Users.ToList());
         }
 
-        public ActionResult Create()
-        {
-            return View(new ApplicationUser());
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> Create(ApplicationUser model)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
-                var result = await UserManager.CreateAsync(user, "motdepasse");
-                if (user.FirstName == "admin")
-                {
-                    await UserManager.AddToRoleAsync(user.Id, "Admin");
-                }
-                else
-                {
-                    await UserManager.AddToRoleAsync(user.Id, "User");
-                }
-
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    // Pour plus d'informations sur l'activation de la confirmation de compte et de la réinitialisation de mot de passe, visitez https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Envoyer un message électronique avec ce lien
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirmez votre compte", "Confirmez votre compte en cliquant <a href=\"" + callbackUrl + "\">ici</a>");
-                    return RedirectToAction("Index");
-                }
-
-            }
-
-            // Si nous sommes arrivés là, un échec s’est produit. Réafficher le formulaire
-            return View(model);
-        }
-
         public ActionResult Delete(string id)
         {
             if (id == null)
